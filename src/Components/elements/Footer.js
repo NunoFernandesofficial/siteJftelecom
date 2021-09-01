3c36b21953abe8a1d974cd5cccba1083387d531d
NunoFernandesofficial <nunomcfernandes12@gmail.com>
2021-09-02 00:23:29 +0100

    third commit add footer

--- src/Components/elements/Footer.js
+++ src/Components/elements/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import '../css/Footer.css'
 
 function Footer() {
     return (
-        <div>
-            sss
-        </div>
+        <div className="Footer-main">
+            <div class="card">
+                <h1>Zona de trabalho</h1>
+                <hr className="linha"></hr>
+                <p>Lisboa</p>
+            </div>
+
+
+            
+            <div class="card">
+                <h1>Email</h1>
+                <hr className="linha"></hr>
+                <p>JorgeFernandes11@gmail.com</p>
+            </div>
+
+
+            <div class="card">
+                <h1>Telemóvel</h1>
+                <hr className="linha"></hr>
+                <p>936071788</p>
+            </div>
+
+         <div className="creditos">
+             <small>Developer: NunoFernandes</small>
+             </div>   
+        </div>   
+        
     )
 }
 
